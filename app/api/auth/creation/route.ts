e9c882506f0753aaae2a08662cb09569c4adad25
kdsking19 <kdsonagara20@gmail.com>
2024-10-15 00:45:00 +0530

    update route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://gothic-trends-pa0giu4yg-kishan-sonagaras-projects.vercel.app/"
+      : "https://gothic-trends.vercel.app/"
   );
 }
