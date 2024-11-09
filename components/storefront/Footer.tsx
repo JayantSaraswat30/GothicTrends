496db6d6768d71bcd7512819e30ce4ee3014dba1
kdsking19 <kdsonagara20@gmail.com>
2024-11-09 23:50:59 +0530

    update Footer

--- components/storefront/Footer.tsx
+++ components/storefront/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
     return (
-      <footer className="mt-16 mb-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-xs leading-5 text-gray-700">
+      <footer className="mt-16 mb-10 max-w-7xl mx-auto px-4 sm:px-4 lg:px-6">
+        <div className="border-t dark:border-white pt-8 sm:mt-20 lg:mt-24">
+          <p className="text-xs leading-5 dark:text-white">
             &copy; 2024 All rights reserved by GothicTrends | Made in India
           </p>
         </div>
